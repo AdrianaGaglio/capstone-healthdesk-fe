96a863bc3e6f164e643d7a5efd8b50b49c60e7e9
Adriana Gaglio <adriana.gaglio@gmail.com>
2025-02-24 12:54:52 +0100

    contact page

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { DoctorService } from '../../services/doctor.service';
+import { iDoctor } from '../../interfaces/idoctor';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.scss'
+  styleUrl: './contacts.component.scss',
 })
 export class ContactsComponent {
+  constructor(private doctorSvc: DoctorService) {}
 
+  doctor!: iDoctor;
+
+  ngOnInit() {
+    this.doctorSvc.doctor$.subscribe((doctor) => {
+      if (doctor) {
+        this.doctor = doctor;
+      }
+    });
+  }
 }
--- src/app/pages/contacts/contacts.module.ts
+++ src/app/pages/contacts/contacts.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { ContactsComponent } from './contacts.component';
-
+import { ContactFormModule } from '../../shared/contact-form/contact-form.module';
+import { NgIconsModule } from '@ng-icons/core';
+import { DoctorAddressModule } from '../../shared/doctor-address/doctor-address.module';
 
 @NgModule({
-  declarations: [
-    ContactsComponent
-  ],
+  declarations: [ContactsComponent],
   imports: [
     CommonModule,
-    ContactsRoutingModule
-  ]
+    ContactsRoutingModule,
+    ContactFormModule,
+    NgIconsModule,
+    DoctorAddressModule,
+  ],
 })
-export class ContactsModule { }
+export class ContactsModule {}
